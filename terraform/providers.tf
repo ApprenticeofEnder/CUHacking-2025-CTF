8d1b7d5234ec7dc13dd5bcd6501b4cc07dedddba
Robert Babaev <github@robertbabaev.tech>
2025-01-29 17:26:56 -0500

    fix terraform deploy not working

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,7 +11,10 @@ terraform {
     key                         = "cuhacking-2025/terraform/terraform.tfstate"
     bucket                      = "rbabaev-terraform-storage"
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
   }
 }
 

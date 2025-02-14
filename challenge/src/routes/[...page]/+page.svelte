ca5201f9e13ea5e9e35adc1d2d1f0abe7743fb38
Robert Babaev <github@robertbabaev.tech>
2025-02-14 01:43:16 -0500

    fix page content

--- challenge/src/routes/[...page]/+page.svelte
+++ challenge/src/routes/[...page]/+page.svelte
@@ -16,8 +16,9 @@
 
 	<p>
 		Here's the sitch: Corp has been planning a new AI slop feature for months. Even got a testbed
-		for it. I've been able to find a way to get past it and into the server itself, but I can't
-		actually make any backdoors lest I get caught. See if you can't exfil the /secret_data directory.
+		for it, though it's been slow going. I've been able to find a way to get past it and into the
+		server itself, but I can't actually make any backdoors lest I get caught. See if you can't exfil
+		the /secret_data directory.
 	</p>
 
 	<p>Here's the code for the endpoint (base64 encoded), they're using SvelteKit under the hood:</p>

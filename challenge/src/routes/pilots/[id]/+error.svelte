267cd8df01e73455989583115898a159a574cdc4
Robert Babaev <github@robertbabaev.tech>
2025-03-11 23:41:39 -0400

    add 404 page for pilots

---
+++ challenge/src/routes/pilots/[id]/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { PilotDetail } from '$lib/components/pilot-detail';
+</script>
+
+<PilotDetail
+	id={-1}
+	name="UNKNOWN"
+	notes="That pilot is not in our database."
+	biography="N/A"
+	classified={false}
+	callsign="UNKNOWN"
+/>
